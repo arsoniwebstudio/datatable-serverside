a48a2f217cc9169e90c7d63a47a762f42c8a9798
manperlop <manperlop1@gmail.com>
2015-03-13 10:30:36 +0100

    added withAlias option

--- src/lib/DtController.php
+++ src/lib/DtController.php
@@ -1,23 +1,27 @@
 <?php
 
 use Illuminate\Routing\Controller;
- 
-abstract class DtController extends Controller {
- 
-  abstract protected function getModel();
-  abstract protected function setDTColums($dataTable);
-  
-  protected function getDT($timestamps = true)
-  {
-  	$model = $this->getModel();
-  	return $model::datatable($timestamps);
-  }
-  
-  public function datatable($timestamps = true)
-  {
-  	$dataTable = $this -> getDT($timestamps);
-  	$dataTable = $this -> setDTColums($dataTable);
-  	return $dataTable->make();
-  }
-  
+
+abstract class DtController extends Controller
+{
+
+    abstract protected function getModel();
+
+    abstract protected function setDTColums($dataTable);
+
+    protected function getDT($timestamps = true)
+    {
+        $model = $this->getModel();
+        return $model::datatable($timestamps);
+    }
+
+    public function datatable($timestamps = true, $withAlias = false)
+    {
+        $dataTable = $this->getDT($timestamps);
+        $dataTable = $this->setDTColums($dataTable);
+        if ($withAlias)
+            $dataTable = $dataTable ->setSearchWithAlias();
+        return $dataTable->make();
+    }
+
 }
